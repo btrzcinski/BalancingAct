935c1c5b472a24e51fabc5c10fe35a8174454fb3
Barnett Trzcinski <btrzcinski@gmail.com>
2020-08-08 15:27:46 -0700

    Standardize displayed times to New York

--- src/app/portfolio/portfolio-quote/portfolio-quote.component.ts
+++ src/app/portfolio/portfolio-quote/portfolio-quote.component.ts
@@ -1,21 +1,18 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input} from '@angular/core';
 import {Quote} from '../../models/quote';
 import {Observable} from 'rxjs';
+import {tz} from 'moment-timezone';
 
 @Component({
   selector: 'portfolio-quote',
   templateUrl: './portfolio-quote.component.html',
   styleUrls: ['./portfolio-quote.component.css']
 })
-export class PortfolioQuoteComponent implements OnInit {
+export class PortfolioQuoteComponent {
 
   @Input()
   public quote$: Observable<Quote>;
 
-  constructor() {
-  }
-
-  ngOnInit(): void {
-  }
+  nycTz = tz('America/New_York').isDST() ? 'EDT' : 'EST';
 
 }
